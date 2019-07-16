452521906f3172a9181febb6c2309306520e0371
Jfleming <Jfleming07712@gmail.com>
2019-07-16 13:47:31 -0400

    Lets Make Vehicles

--- VehicleFactory/Program.cs
+++ VehicleFactory/Program.cs
@@ -6,7 +6,7 @@ namespace VehicleFactory
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Lets Make Vehicles");
         }
     }
 }
