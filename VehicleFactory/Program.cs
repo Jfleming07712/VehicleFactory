575a9f4b2297fa98a97308523bf79497fde81cfa
Jfleming <Jfleming07712@gmail.com>
2019-07-16 13:46:09 -0400

    Add project files.

---
+++ VehicleFactory/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VehicleFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
